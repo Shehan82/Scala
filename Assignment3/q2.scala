10571a78d8a6e7d3c62c63f7d9a38aa557ab9ab2
shehan82 <shehansandeepa82@gmail.com>
2020-05-28 23:47:01 +0530

    make changes

--- Assignment3/q2.scala
+++ Assignment3/q2.scala
@@ -7,18 +7,32 @@ object q2
 
 	}
 
-	def divide(a:Int,b:Int=2):Boolean= b match
-	{
-		case x if(a!=x && a%x==0) => false
-		case x if(a<=x) => true
-		case x => divide(a,x+1)
-	}
+	// def divide(a:Int,b:Int=2):Boolean= b match
+	// {
+	// 	case x if(a!=x && a%x==0) => false
+	// 	case x if(a<=x) => true
+	// 	case x => divide(a,x+1)
+	// }
 
-	def isPrime(p:Int):Boolean=p match 
+	// def isPrime(p:Int):Boolean=p match 
+	// {
+	// 	case x if(x==2) => true
+	// 	case x if(x==1) => false
+	// 	case x => divide(x) 
+	// }
+
+	def gcd(n:Int,t:Int):Int=t match
 	{
-		case x if(x==2) => true
-		case x if(x==1) => false
-		case x => divide(x) 
+		case 0 => n
+		case x if(x>n) => gcd(x,n)
+		case x => gcd(x,n%x)
+	}
+	def isPrime(a:Int,b:Int=2):Boolean= b match 
+	{
+		case x if(a==1) => false
+		case x if(x==a) => true
+		case x if gcd(a,x)>1 => false
+		case x => isPrime(a,x+1)
 	}
 
 	def printPrime(r:Int,n:Int=2):Unit=
