af037cc7f6d574d36badcd3874ab4147c25c2ce4
shehan82 <shehansandeepa82@gmail.com>
2020-05-29 00:28:33 +0530

    make changes

--- Assignment3/q3.scala
+++ Assignment3/q3.scala
@@ -2,13 +2,15 @@ object q3
 {
 	def main(args: Array[String])
 	{
-		println(sum(10));
+		println(sum(4));
 	}
 
-	def sum(n:Int,p:Int=1,t:Int=1):Int= n match
+	def sum(n:Int,p:Int=0):Int=n match
 	{
-		case x if(p==n) => t
-		case x => sum(x,p+1,t+p+1)
+		case x if(x==0) => p
+		case x => sum(n-1,p+n)
 	}
+
+
 	
 }
